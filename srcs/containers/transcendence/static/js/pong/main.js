123821a9028e02f6ac32ea1e5d1827bd2303a2ef
leon <leonjerineck@gmail.com>
2024-07-24 14:13:20 +0200

    test websocket

--- srcs/containers/transcendence/static/js/pong/main.js
+++ srcs/containers/transcendence/static/js/pong/main.js
@@ -1,5 +1,30 @@
 function main()
 {
+
+    let socket = new WebSocket('ws://localhost:8000/ws/game/testroom/');
+
+    socket.onopen = function(e) {
+      console.log("[open] Connection established");
+      console.log("Sending to server");
+      socket.send(JSON.stringify({'message': 'Hello Server!'}));
+    };
+
+    socket.onmessage = function(event) {
+      console.log(`[message] Data received from server: ${event.data}`);
+    };
+
+    socket.onclose = function(event) {
+      if (event.wasClean) {
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+      } else {
+        console.log('[close] Connection died');
+      }
+    };
+
+    socket.onerror = function(error) {
+      console.log(`[error] ${error.message}`);
+    };
+
     let canevas = document.getElementById("canv");
 
     let utils = {
