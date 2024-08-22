1e6ba4efe548457f11f226c3c9cf3ac017f18066
leon <leonjerineck@gmail.com>
2024-08-22 16:18:44 +0200

    game display OK + comment

--- srcs/containers/transcendence/static/js/pong/socket_response.js
+++ srcs/containers/transcendence/static/js/pong/socket_response.js
@@ -54,6 +54,7 @@ function responsePong() {
                 to_unspecified_page('game/');
             else if (data.action === 'start_game')
             {
+                //supp lobby.css
                 document.getElementById('lobby_div').remove();
                 main_game();
             }
