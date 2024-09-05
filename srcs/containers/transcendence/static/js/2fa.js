b8f0a89bbb7da9e92aa1d68d6a25415febf544e9
Neo dans Matrix <neodansmatrix@neos-mbp.home>
2024-09-05 11:45:47 +0200

    jcrois mon erreur se fait catch par un autre script

--- srcs/containers/transcendence/static/js/2fa.js
+++ srcs/containers/transcendence/static/js/2fa.js
@@ -19,41 +19,48 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 if (document.getElementById('otp-form')) {
-	document.getElementById('otp-form').addEventListener('submit', function(event) {
-		event.preventDefault(); // Prevent the form from submitting the traditional way
-		
-		const form = event.target;
-		const formData = new FormData(form);
-		console.log("sending fetch to check otp-form");
-		fetch('account/redirect/setup/', {
-			method: 'POST',
-			headers: {
-				'X-CSRFToken': '{{ csrf_token }}',
-			},
-			body: formData,
-		})
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json();
-		})
-		.then(data => {
-			if (data.success) {
-				window.location.href = data.redirect_url;
-			} else {
-				if (data.redirect_url) {
-					window.location.href = data.redirect_url;
-				}
-				console.log(data.error);
-			}
-		})
-		.catch(error => {
-			console.error('Error:', error);
-			const errorMessageElement = document.getElementById('error-message');
-			if (errorMessageElement) {
-				errorMessageElement.textContent = 'An unexpected error occurred.';
-			}
-		});		
-	});
+    document.getElementById('otp-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from submitting the traditional way
+
+        const form = event.target;
+        const formData = new FormData(form);
+        console.log("sending fetch to check otp-form");
+
+        fetch('account/redirect/setup/', {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': '{{ csrf_token }}',
+            },
+            body: formData,
+        })
+        .then(response => {
+            console.log("catched otp-form response...");
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                window.location.href = data.redirect_url;
+            } else {
+                // If there's an error, display it in the error-message div
+                const errorMessageElement = document.getElementById('error-message');
+                if (errorMessageElement && data.error) {
+                    errorMessageElement.textContent = data.error;
+                }
+
+                if (data.redirect_url) {
+                    window.location.href = data.redirect_url;
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const errorMessageElement = document.getElementById('error-message');
+            if (errorMessageElement) {
+                errorMessageElement.textContent = 'An unexpected error occurred.';
+            }
+        });
+    });
 }
