61dd3cc8f52f85349a756d0dfcdcc06f67dddf7d
Samuel Munio <smunio@k2r1p3.42mulhouse.fr>
2024-09-09 16:02:41 +0200

    need some fixes on 2fa SPA/need to code the login checker

--- srcs/containers/transcendence/accounts/views.py
+++ srcs/containers/transcendence/accounts/views.py
@@ -10,33 +10,50 @@ from django.urls import reverse
 from django.http import JsonResponse
 from django_otp.forms import OTPTokenForm
 from django_otp.plugins.otp_totp.models import TOTPDevice
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 @login_required
 def redirect_to_2fa_setup(request):
     print("User:", request.user)
+
+    try:
+        device = TOTPDevice.objects.get(user=request.user, name='default')
+    except MultipleObjectsReturned:
+        device = TOTPDevice.objects.filter(user=request.user, name='default').first()
+        print("Multiple devices found. Using the first one.")
+    except ObjectDoesNotExist:
+        device = None
+        print("Device does not exist. Creating a new one.")
+        device = TOTPDevice.objects.create(user=request.user, name='default')
+    
     if request.method == 'POST':
-        form = OTPTokenForm(request.POST)
+        form = OTPTokenForm(data=request.POST, user=request.user)
         
         if form.is_valid():
+            print("form_is_valid")
             token = form.cleaned_data['otp_token']
-            if request.user.otp_device.verify_token(token):
-                return redirect('/game/')
+            print("Device:", device, "Token:", token)
+    
+            if device and device.verify_token(token):
+                return JsonResponse({'success': True, 'redirect_url': '/game/'})
             else:
-                form.add_error(None, 'Invalid OTP token.')
+                return JsonResponse({'success': False, 'error': 'Invalid OTP token.'})
         else:
-            form.add_error(None, 'Invalid form submission.')
+            print("Form errors:", form.errors)
+            return JsonResponse({'success': False, 'error': 'Invalid form submission.'})
     
     else:
         form = OTPTokenForm(user=request.user)
-    
-    device, created = TOTPDevice.objects.get_or_create(user=request.user, name='default')
-    
-    qr_code_img = qrcode.make(device.config_url)
-    buffer = BytesIO()
-    qr_code_img.save(buffer)
-    buffer.seek(0)
-    encoded_img = b64encode(buffer.read()).decode()
-    qr_code_data = f'data:image/png;base64,{encoded_img}'
+    if device:
+        qr_code_img = qrcode.make(device.config_url)
+        buffer = BytesIO()
+        qr_code_img.save(buffer)
+        buffer.seek(0)
+        encoded_img = b64encode(buffer.read()).decode()
+        qr_code_data = f'data:image/png;base64,{encoded_img}'
+    else:
+        qr_code_data = None
+        print("Error: No device")       
     
     return render(request, 'setup.html', {'form': form, 'qr_url': qr_code_data})
 
