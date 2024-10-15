244b27f9391d11afcce7ddfdc17d9ac5c441fe79
leon <leonjerineck@gmail.com>
2024-10-15 15:34:45 +0200

    save to update

--- srcs/containers/transcendence/entrypoint.prod.sh
+++ srcs/containers/transcendence/entrypoint.prod.sh
@@ -21,6 +21,14 @@ from django.contrib.auth import get_user_model;
 User = get_user_model();
 if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
     User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+if not User.objects.filter(username='ludo').exists():
+    ludo = User.objects.create_user(username='ludo', email='ludo@maildeludo.com', password='fefe1fefe')
+if not User.objects.filter(username='leon').exists():
+    leon = User.objects.create_user(username='leon', email='leon@maildeludo.com', password='fefe1fefe')
+if not User.objects.filter(username='basi').exists():
+    basi = User.objects.create_user(username='basi', email='basi@maildeludo.com', password='fefe1fefe')
+if not User.objects.filter(username='anto').exists():
+    anto = User.objects.create_user(username='anto', email='anto@maildeludo.com', password='fefe1fefe')
 "
 fi
 
